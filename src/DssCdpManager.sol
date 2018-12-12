046e555fcc45ded3ec74b5e05d400beaeeacb671
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-12-12 13:54:31 -0300

    Initial commit

--- src/DssCdpManager.sol
+++ src/DssCdpManager.sol
@@ -1,4 +1,98 @@
-pragma solidity ^0.4.24;
+/// DssCdpManager.sol
+
+// Copyright (C) 2018 Rain <rainbreak@riseup.net>
+// Copyright (C) 2018 Gonzalo Balabasquer <gbalabasquer@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity >= 0.5.0;
+
+contract PitLike {
+    function frob(bytes32, bytes32, int256, int256) public;
+    function vat() public returns (VatLike);
+}
+
+contract VatLike {
+    function dai(bytes32) public returns (uint);
+}
+
+contract DaiMoveLike {
+    function move(bytes32, bytes32, uint) public;
+}
 
 contract DssCdpManager {
+    mapping (bytes32 => mapping (bytes12 => address)) public cdps;
+    mapping (address => mapping (address => bool)) public allows;
+    mapping (bytes32 => uint96) public cdpsi;
+
+    uint256 constant ONE = 10 ** 27;
+
+    event Note(
+        bytes4   indexed  sig,
+        bytes32  indexed  foo,
+        bytes32  indexed  bar,
+        bytes32  indexed  too,
+        bytes             fax
+    ) anonymous;
+
+    modifier note {
+        bytes32 foo;
+        bytes32 bar;
+        bytes32 too;
+        assembly {
+            foo := calldataload(4)
+            bar := calldataload(36)
+            too := calldataload(68)
+        }
+        emit Note(msg.sig, foo, bar, too, msg.data);
+        _;
+    }
+
+    modifier isAllowed(bytes32 ilk, bytes12 cdp) {
+        require(msg.sender == cdps[ilk][cdp] || allows[cdps[ilk][cdp]][msg.sender], "");
+        _;
+    }
+
+    function allow(address guy, bool ok) public { allows[msg.sender][guy] = ok; }
+
+    function open(bytes32 ilk) public returns (bytes12 cdp) {
+        cdp = open(ilk, msg.sender);
+    }
+
+    function open(bytes32 ilk, address guy) public note returns (bytes12 cdp) {
+        cdpsi[ilk] ++;
+        cdp = bytes12(cdpsi[ilk]);
+        cdps[ilk][cdp] = guy;
+    }
+
+    function move(bytes32 ilk, bytes12 cdp, address dst) public note isAllowed(ilk, cdp) {
+        cdps[ilk][cdp] = dst;
+    }
+
+    function getUrn(bytes12 cdp) public view returns (bytes32 urn) {
+        assembly {
+            let p := mload(0x40)
+            mstore(p, address)
+            mstore(p, mload(add(p, 0xc)))
+            mstore(add(p, 0x14), cdp)
+            urn := mload(p)
+        }
+    }
+
+    function frob(address pit, address daiMove, bytes32 ilk, bytes12 cdp, int dink, int dart, bytes32 dst) public /*note*/ isAllowed(ilk, cdp) {
+        bytes32 urn = getUrn(cdp);
+        PitLike(pit).frob(urn, ilk, dink, dart);
+        DaiMoveLike(daiMove).move(urn, dst, PitLike(pit).vat().dai(urn) / ONE);
+    }
 }
--- src/DssCdpManager.t.sol
+++ src/DssCdpManager.t.sol
@@ -1,21 +1,95 @@
-pragma solidity ^0.4.24;
+pragma solidity >= 0.5.0;
 
-import "ds-test/test.sol";
-
+import { DssDeployTest } from "dss-deploy/DssDeploy.t.sol";
 import "./DssCdpManager.sol";
 
-contract DssCdpManagerTest is DSTest {
+contract FakeUser {
+    function doMove(DssCdpManager manager, bytes32 ilk, bytes12 cdp, address dst) public {
+        manager.move(ilk, cdp, dst);
+    }
+
+    function doFrob(DssCdpManager manager, address pit, address daiMove, bytes32 ilk, bytes12 cdp, int dink, int dart, bytes32 dst) public {
+        manager.frob(pit, daiMove, ilk, cdp, dink, dart, dst);
+    }
+}
+
+contract DssCdpManagerTest is DssDeployTest {
     DssCdpManager manager;
+    FakeUser user;
 
     function setUp() public {
+        super.setUp();
         manager = new DssCdpManager();
+        user = new FakeUser();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function _getUrn(bytes12 cdp) internal returns (bytes32 urn) {
+        bytes20 addr = bytes20(address(manager));
+        assembly {
+            let p := mload(0x40)
+            mstore(p, addr)
+            mstore(add(p, 0x14), cdp)
+            urn := mload(p)
+        }
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function testManagerOpenCDP() public {
+        bytes12 cdp = manager.open("ETH");
+        assertEq(bytes32(cdp), bytes32(bytes12(uint96(1))));
+        assertEq(manager.cdps("ETH", cdp), address(this));
+    }
+
+    function testManagerOpenCDPOtherAddress() public {
+        bytes12 cdp = manager.open("ETH", address(123));
+        assertEq(manager.cdps("ETH", cdp), address(123));
+    }
+
+    function testManagerTransferCDP() public {
+        bytes12 cdp = manager.open("ETH");
+        manager.move("ETH", cdp, address(123));
+        assertEq(manager.cdps("ETH", cdp), address(123));
+    }
+
+    function testManagerTransferAllowed() public {
+        bytes12 cdp = manager.open("ETH");
+        manager.allow(address(user), true);
+        user.doMove(manager, "ETH", cdp, address(123));
+        assertEq(manager.cdps("ETH", cdp), address(123));
+    }
+
+    function testFailManagerTransferNotAllowed() public {
+        bytes12 cdp = manager.open("ETH");
+        user.doMove(manager, "ETH", cdp, address(123));
+    }
+
+    function testFailManagerTransferNotAllowed2() public {
+        bytes12 cdp = manager.open("ETH");
+        manager.allow(address(user), true);
+        manager.allow(address(user), false);
+        user.doMove(manager, "ETH", cdp, address(123));
+    }
+
+    function testManagerFrob() public {
+        deploy();
+        bytes12 cdp = manager.open("ETH");
+        ethJoin.join.value(1 ether)(manager.getUrn(cdp));
+        manager.frob(address(pit), address(daiMove), "ETH", cdp, 1 ether, 50 ether, bytes32(bytes20(address(this))));
+        assertEq(vat.dai(bytes32(bytes20(address(this)))), 50 ether * ONE);
+    }
+
+    function testManagerFrobAllowed() public {
+        deploy();
+        bytes12 cdp = manager.open("ETH");
+        ethJoin.join.value(1 ether)(manager.getUrn(cdp));
+        manager.allow(address(user), true);
+        user.doFrob(manager, address(pit), address(daiMove), "ETH", cdp, 1 ether, 50 ether, bytes32(bytes20(address(this))));
+        assertEq(vat.dai(bytes32(bytes20(address(this)))), 50 ether * ONE);
+    }
+
+    function testFailManagerFrobNotAllowed() public {
+        deploy();
+        bytes12 cdp = manager.open("ETH");
+        ethJoin.join.value(1 ether)(manager.getUrn(cdp));
+        user.doFrob(manager, address(pit), address(daiMove), "ETH", cdp, 1 ether, 50 ether, bytes32(bytes20(address(this))));
     }
 }
