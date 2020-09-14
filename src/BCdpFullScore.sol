351388221bb1e1df0753265c31fa94a30c2d2bed
Jitendra Chittoda <jcchittoda@gmail.com>
2020-09-14 17:22:22 +0530

    moved `slashScore` in BCdpScore

--- src/BCdpFullScore.sol
+++ src/BCdpFullScore.sol
@@ -2,10 +2,6 @@ pragma solidity ^0.5.12;
 
 import { BCdpScore } from "./BCdpScore.sol";
 
-contract ScoreConnectorLike {
-    function left(uint cdp) public returns (uint);
-}
-
 contract BCdpFullScore is BCdpScore {
 
     function inkAsset(bytes32 ilk) public pure returns(bytes32) {
@@ -20,33 +16,6 @@ contract BCdpFullScore is BCdpScore {
     function updateScore(uint cdp, bytes32 ilk, int dink, int dart, uint time) external onlyManager {
         updateScore(user(cdp), inkAsset(ilk), dink, time);
         updateScore(user(cdp), artAsset(ilk), dart, time);
-    }
-
-    function slashScore(uint maliciousCdp) external {
-        address urn = manager.urns(maliciousCdp);
-        bytes32 ilk = manager.ilks(maliciousCdp);
-
-        (, uint realArt) = manager.vat().urns(ilk, urn);
-
-        bytes32 maliciousUser = user(maliciousCdp);
-        bytes32 asset = artAsset(ilk);
-
-        uint left = ScoreConnectorLike(address(manager)).left(maliciousCdp);
-        uint time = 0;
-        int dart = 0;
-
-        uint calculatedArt = getCurrentBalance(maliciousUser, asset);
-        if(left > 0) {
-            if(left > start) time = left;
-            dart = -int(calculatedArt);
-        } else {
-            require(realArt < calculatedArt, "slashScore-cdp-is-ok");
-            dart = int(realArt) - int(calculatedArt);
-            time = sub(now, 30 days);
-            if(time < start) time = start;
-        }
-        
-        updateScore(maliciousUser, asset, dart, time);
     }
 
     function getInkScore(uint cdp, bytes32 ilk, uint time, uint spinStart) public view returns(uint) {
--- src/BCdpScore.sol
+++ src/BCdpScore.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.5.12;
 import { ScoringMachine } from "../user-rating/contracts/score/ScoringMachine.sol";
 import { BCdpManager } from "./BCdpManager.sol";
 
+contract ScoreConnectorLike {
+    function left(uint cdp) public returns (uint);
+}
+
 contract BCdpScore is ScoringMachine {
     BCdpManager public manager;
 
@@ -26,6 +30,33 @@ contract BCdpScore is ScoringMachine {
     function updateScore(uint cdp, bytes32 ilk, int dink, int dart, uint time) external onlyManager {
         dink; // shh compiler warning
         updateScore(user(cdp), artAsset(ilk), dart, time);
+    }
+
+    function slashScore(uint maliciousCdp) external {
+        address urn = manager.urns(maliciousCdp);
+        bytes32 ilk = manager.ilks(maliciousCdp);
+
+        (, uint realArt) = manager.vat().urns(ilk, urn);
+
+        bytes32 maliciousUser = user(maliciousCdp);
+        bytes32 asset = artAsset(ilk);
+
+        uint left = ScoreConnectorLike(address(manager)).left(maliciousCdp);
+        uint time = 0;
+        int dart = 0;
+
+        uint calculatedArt = getCurrentBalance(maliciousUser, asset);
+        if(left > 0) {
+            if(left > start) time = left;
+            dart = -int(calculatedArt);
+        } else {
+            require(realArt < calculatedArt, "slashScore-cdp-is-ok");
+            dart = int(realArt) - int(calculatedArt);
+            time = sub(now, 30 days);
+            if(time < start) time = start;
+        }
+        
+        updateScore(maliciousUser, asset, dart, time);
     }
 
     function getArtScore(uint cdp, bytes32 ilk, uint time, uint spinStart) public view returns(uint) {
